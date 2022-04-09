77eb9e20eb53c4fdc647e85946020401ce700150
Viraj Patidar <vrj381@gmail.com>
2022-04-09 10:19:10 +0530

    react toast

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import Home from './pages/Home';
 import EditorPage from './pages/EditorPage';
 
 function App() {
+    
     return (
         <>
+            <div>
+                <Toaster
+                    position="top-center"
+                    toastOptions={{
+                        success: {
+                            theme: {
+                                primary: '#4aed88',
+                            },
+                        },
+                    }}
+                ></Toaster>
+            </div>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
