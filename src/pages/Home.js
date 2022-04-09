3d40cd8bfb5b931008df20d103c4282ad8582ba2
Viraj Patidar <vrj381@gmail.com>
2022-04-09 10:15:01 +0530

    form state handled

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate();
 
+    const [roomId, setRoomId] = useState('');
+    const [username, setUsername] = useState('');
+    
+    const createNewRoom = (e) => {
+        e.preventDefault();
+        const id = uuidV4();
+        setRoomId(id);
+    };
+
+    const joinRoom = () => {
+        if (!roomId || !username) {
+            return;
+        }
+
+        // Redirect
+        navigate(`/editor/${roomId}`, {
+            state: {
+                username,
+            },
+        });
+    };
+
+    const handleInputEnter = (e) => {
+        if (e.code === 'Enter') {
+            joinRoom();
+        }
+    };
     return (
         <div className="homePageWrapper">
             <div className="formWrapper">
@@ -16,18 +46,25 @@ const Home = () => {
                         type="text"
                         className="inputBox"
                         placeholder="ROOM ID"
+                        onChange={(e) => setRoomId(e.target.value)}
+                        value={roomId}
+                        onKeyUp={handleInputEnter}
                     />
                     <input
                         type="text"
                         className="inputBox"
                         placeholder="USERNAME"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
+                        onKeyUp={handleInputEnter}
                     />
-                    <button className="btn joinBtn" >
+                    <button className="btn joinBtn" onClick={joinRoom}>
                         Join
                     </button>
                     <span className="createInfo">
-                        If you don't have an invite then create a&nbsp;
+                        If you don't have an invite then create &nbsp;
                         <a
+                            onClick={createNewRoom}
                             href=""
                             className="createNewBtn"
                         >
