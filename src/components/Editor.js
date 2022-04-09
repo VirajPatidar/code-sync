c7b3a2e09058f01b57bad69529bfd29784ee6f86
Viraj Patidar <vrj381@gmail.com>
2022-04-09 13:09:58 +0530

    multi language support

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,24 +1,59 @@
 import React, { useState, useEffect } from 'react';
+import { language } from '../../src/atoms';
+import { useRecoilValue } from 'recoil';
+
+// CODE MIRROR
 import Codemirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/monokai.css';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/addon/edit/closetag';
-import 'codemirror/addon/edit/closebrackets';
-import 'codemirror/addon/search/search.js';
-import 'codemirror/addon/search/searchcursor.js';
-import 'codemirror/addon/search/jump-to-line.js';
-import 'codemirror/addon/dialog/dialog.js';
-import 'codemirror/addon/dialog/dialog.css';
+
+// theme
+    import 'codemirror/theme/monokai.css';
+
+// modes
+    import 'codemirror/mode/clike/clike';
+    import 'codemirror/mode/css/css';
+    import 'codemirror/mode/dart/dart';
+    import 'codemirror/mode/django/django';
+    import 'codemirror/mode/dockerfile/dockerfile';
+    import 'codemirror/mode/go/go';
+    import 'codemirror/mode/htmlmixed/htmlmixed';
+    import 'codemirror/mode/javascript/javascript';
+    import 'codemirror/mode/jsx/jsx';
+    import 'codemirror/mode/markdown/markdown';
+    import 'codemirror/mode/php/php';
+    import 'codemirror/mode/python/python';
+    import 'codemirror/mode/r/r';
+    import 'codemirror/mode/rust/rust';
+    import 'codemirror/mode/ruby/ruby';
+    import 'codemirror/mode/sass/sass';
+    import 'codemirror/mode/shell/shell';
+    import 'codemirror/mode/sql/sql';
+    import 'codemirror/mode/swift/swift';
+    import 'codemirror/mode/xml/xml';
+    import 'codemirror/mode/yaml/yaml';
+
+// features
+    import 'codemirror/addon/edit/closetag';
+    import 'codemirror/addon/edit/closebrackets';
+    import 'codemirror/addon/scroll/simplescrollbars.css';
+
+//search
+    import 'codemirror/addon/search/search.js';
+    import 'codemirror/addon/search/searchcursor.js';
+    import 'codemirror/addon/search/jump-to-line.js';
+    import 'codemirror/addon/dialog/dialog.js';
+    import 'codemirror/addon/dialog/dialog.css';
 
 const Editor = () => {
+
+    const lang = useRecoilValue(language);
 
     useEffect(() => {
         async function init() {
             Codemirror.fromTextArea(
                 document.getElementById('realtimeEditor'),
                 {
-                    mode: { name: 'javascript', json: true },
+                    mode: { name: lang },
                     theme: 'monokai',
                     autoCloseTags: true,
                     autoCloseBrackets: true,
@@ -27,12 +62,10 @@ const Editor = () => {
             );
         }
         init();
-    }, []);
+    }, [lang]);
 
     return (
-        <>
             <textarea id="realtimeEditor"></textarea>
-        </>
     );
 };
 
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import Client from '../components/Client';
 import Editor from '../components/Editor'
+import { language } from '../../src/atoms';
+import { useRecoilState } from 'recoil';
 
 const EditorPage = () => {
+
+    const [lang, setLang] = useRecoilState(language);
 
     const [clients, setClients] = useState([
         { socketId: 1, username: 'Viraj' },
@@ -33,6 +37,33 @@ const EditorPage = () => {
                         ))}
                     </div>
                 </div>
+
+                <label>
+                    Select Language:
+                    <select value={lang} onChange={(e) => {setLang(e.target.value); window.location.reload();}} className="seLang">
+                        <option value="clike">C / C++ / C#</option>
+                        <option value="css">CSS</option>
+                        <option value="dart">Dart</option>
+                        <option value="django">Django</option>
+                        <option value="dockerfile">Dockerfile</option>
+                        <option value="go">Go</option>
+                        <option value="htmlmixed">HTML-mixed</option>
+                        <option value="javascript">JavaScript</option>
+                        <option value="jsx">JSX</option>
+                        <option value="markdown">Markdown</option>
+                        <option value="php">PHP</option>
+                        <option value="python">Python</option>
+                        <option value="r">R</option>
+                        <option value="rust">Rust</option>
+                        <option value="ruby">Ruby</option>
+                        <option value="sass">Sass</option>
+                        <option value="shell">Shell</option>
+                        <option value="sql">SQL</option>
+                        <option value="swift">Swift</option>
+                        <option value="xml">XML</option>
+                        <option value="yaml">yaml</option>
+                    </select>
+                </label>
 
                 <button className="btn copyBtn">
                     Copy ROOM ID
