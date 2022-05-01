59bba4bbfbbd9f7547a662873d883eccccd1a474
Viraj Patidar <vrj381@gmail.com>
2022-05-01 12:21:59 +0530

    client socket connection

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import toast from 'react-hot-toast';
 import Client from '../components/Client';
 import Editor from '../components/Editor'
 import { language } from '../../src/atoms';
 import { useRecoilState } from 'recoil';
+import ACTIONS from '../Actions';
+import { initSocket } from '../socket';
+import {
+    useLocation,
+    useNavigate,
+    Navigate,
+    useParams,
+} from 'react-router-dom';
 
 const EditorPage = () => {
 
@@ -15,6 +24,73 @@ const EditorPage = () => {
         { socketId: 4, username: 'Tony' },
         { socketId: 5, username: 'Krish' },
     ]);
+
+
+    const socketRef = useRef(null);
+    const codeRef = useRef(null);
+    const location = useLocation();
+    const { roomId } = useParams();
+    const reactNavigator = useNavigate();
+
+    useEffect(() => {
+        const init = async () => {
+            socketRef.current = await initSocket();
+            socketRef.current.on('connect_error', (err) => handleErrors(err));
+            socketRef.current.on('connect_failed', (err) => handleErrors(err));
+
+            function handleErrors(e) {
+                console.log('socket error', e);
+                toast.error('Socket connection failed, try again later.');
+                reactNavigator('/');
+            }
+
+            socketRef.current.emit(ACTIONS.JOIN, {
+                roomId,
+                username: location.state?.username,
+            });
+
+            // Listening for joined event
+            socketRef.current.on(
+                ACTIONS.JOINED,
+                ({ clients, username, socketId }) => {
+                    if (username !== location.state?.username) {
+                        toast.success(`${username} joined the room.`);
+                        console.log(`${username} joined`);
+                    }
+                    setClients(clients);
+                    socketRef.current.emit(ACTIONS.SYNC_CODE, {
+                        code: codeRef.current,
+                        socketId,
+                    });
+                }
+            );
+
+            // Listening for disconnected
+            socketRef.current.on(
+                ACTIONS.DISCONNECTED,
+                ({ socketId, username }) => {
+                    toast.success(`${username} left the room.`);
+                    setClients((prev) => {
+                        return prev.filter(
+                            (client) => client.socketId !== socketId
+                        );
+                    });
+                }
+            );
+        };
+        init();
+        return () => {
+            socketRef.current.disconnect();
+            socketRef.current.off(ACTIONS.JOINED);
+            socketRef.current.off(ACTIONS.DISCONNECTED);
+        };
+    }, []);
+
+
+    if (!location.state) {
+        return <Navigate to="/" />;
+    }
+
 
     return (
         <div className="mainWrap">
@@ -40,7 +116,7 @@ const EditorPage = () => {
 
                 <label>
                     Select Language:
-                    <select value={lang} onChange={(e) => {setLang(e.target.value); window.location.reload();}} className="seLang">
+                    <select value={lang} onChange={(e) => { setLang(e.target.value); window.location.reload(); }} className="seLang">
                         <option value="clike">C / C++ / C#</option>
                         <option value="css">CSS</option>
                         <option value="dart">Dart</option>
