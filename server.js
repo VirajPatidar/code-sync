7ecdafe0f0589c78b6728efedcdab9dbb18a43d1
Viraj Patidar <vrj381@gmail.com>
2022-05-01 21:23:29 +0530

    minor fix

--- server.js
+++ server.js
@@ -72,5 +72,5 @@ io.on('connection', (socket) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
