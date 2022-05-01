9ca2272c9ed2d55ee93a851b03783b9e630bcbaf
Viraj Patidar <vrj381@gmail.com>
2022-05-01 14:22:45 +0530

    deployment

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 
+app.use(express.static('build'));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 const userSocketMap = {};
