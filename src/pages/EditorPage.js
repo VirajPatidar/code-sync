86cd8aac59525b039d975cffaaf6bd75b34a3a2f
Viraj Patidar <vrj381@gmail.com>
2022-05-01 20:40:55 +0530

    change theme feature added

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import toast from 'react-hot-toast';
 import Client from '../components/Client';
 import Editor from '../components/Editor'
-import { language } from '../../src/atoms';
+import { language, cmtheme } from '../../src/atoms';
 import { useRecoilState } from 'recoil';
 import ACTIONS from '../Actions';
 import { initSocket } from '../socket';
@@ -16,6 +16,7 @@ import {
 const EditorPage = () => {
 
     const [lang, setLang] = useRecoilState(language);
+    const [them, setThem] = useRecoilState(cmtheme);
 
     const [clients, setClients] = useState([]);
 
@@ -148,6 +149,75 @@ const EditorPage = () => {
                         <option value="swift">Swift</option>
                         <option value="xml">XML</option>
                         <option value="yaml">yaml</option>
+                    </select>
+                </label>
+
+                <label>
+                    Select Theme:
+                    <select value={them} onChange={(e) => { setThem(e.target.value); window.location.reload(); }} className="seLang">
+                        <option value="default">default</option>
+                        <option value="3024-day">3024-day</option>
+                        <option value="3024-night">3024-night</option>
+                        <option value="abbott">abbott</option>
+                        <option value="abcdef">abcdef</option>
+                        <option value="ambiance">ambiance</option>
+                        <option value="ayu-dark">ayu-dark</option>
+                        <option value="ayu-mirage">ayu-mirage</option>
+                        <option value="base16-dark">base16-dark</option>
+                        <option value="base16-light">base16-light</option>
+                        <option value="bespin">bespin</option>
+                        <option value="blackboard">blackboard</option>
+                        <option value="cobalt">cobalt</option>
+                        <option value="colorforth">colorforth</option>
+                        <option value="darcula">darcula</option>
+                        <option value="duotone-dark">duotone-dark</option>
+                        <option value="duotone-light">duotone-light</option>
+                        <option value="eclipse">eclipse</option>
+                        <option value="elegant">elegant</option>
+                        <option value="erlang-dark">erlang-dark</option>
+                        <option value="gruvbox-dark">gruvbox-dark</option>
+                        <option value="hopscotch">hopscotch</option>
+                        <option value="icecoder">icecoder</option>
+                        <option value="idea">idea</option>
+                        <option value="isotope">isotope</option>
+                        <option value="juejin">juejin</option>
+                        <option value="lesser-dark">lesser-dark</option>
+                        <option value="liquibyte">liquibyte</option>
+                        <option value="lucario">lucario</option>
+                        <option value="material">material</option>
+                        <option value="material-darker">material-darker</option>
+                        <option value="material-palenight">material-palenight</option>
+                        <option value="material-ocean">material-ocean</option>
+                        <option value="mbo">mbo</option>
+                        <option value="mdn-like">mdn-like</option>
+                        <option value="midnight">midnight</option>
+                        <option value="monokai">monokai</option>
+                        <option value="moxer">moxer</option>
+                        <option value="neat">neat</option>
+                        <option value="neo">neo</option>
+                        <option value="night">night</option>
+                        <option value="nord">nord</option>
+                        <option value="oceanic-next">oceanic-next</option>
+                        <option value="panda-syntax">panda-syntax</option>
+                        <option value="paraiso-dark">paraiso-dark</option>
+                        <option value="paraiso-light">paraiso-light</option>
+                        <option value="pastel-on-dark">pastel-on-dark</option>
+                        <option value="railscasts">railscasts</option>
+                        <option value="rubyblue">rubyblue</option>
+                        <option value="seti">seti</option>
+                        <option value="shadowfox">shadowfox</option>
+                        <option value="solarized">solarized</option>
+                        <option value="the-matrix">the-matrix</option>
+                        <option value="tomorrow-night-bright">tomorrow-night-bright</option>
+                        <option value="tomorrow-night-eighties">tomorrow-night-eighties</option>
+                        <option value="ttcn">ttcn</option>
+                        <option value="twilight">twilight</option>
+                        <option value="vibrant-ink">vibrant-ink</option>
+                        <option value="xq-dark">xq-dark</option>
+                        <option value="xq-light">xq-light</option>
+                        <option value="yeti">yeti</option>
+                        <option value="yonce">yonce</option>
+                        <option value="zenburn">zenburn</option>
                     </select>
                 </label>
 
